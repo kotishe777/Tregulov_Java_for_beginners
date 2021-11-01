5b31f3db9ad3d83e0ef939def7bf3d4959828ff9
kcherkashyna <80709310+kcherkashyna@users.noreply.github.com>
2021-11-01 23:16:43 +0200

    Changes 01.11.2021

--- src/main/java/lesson27/TryCatchLearning2.java
+++ src/main/java/lesson27/TryCatchLearning2.java
@@ -14,20 +14,49 @@ public class TryCatchLearning2 {
             FileInputStream fis = new FileInputStream(f);
             fis.read();
             System.out.println("Bye");
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("The exception is caught:" + e);
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("The exception is caught:" + e);
-        }
-        finally {
+        } finally {
             System.out.println("This is finally block");
         }
-
         System.out.println("These kode doesn't relate to exception");
     }
 
+    void def() throws FileNotFoundException {
+        File f = new File("C:\\Users\\kateryna.cherkashyna\\IdeaProjects\\Java_Learning\\src\\main\\java\\lesson27\\test10.txt");
+
+        FileInputStream fis = new FileInputStream(f);
+        System.out.println("These kode doesn't relate to exception");
+    }
+
+    void ghi() {
+        System.out.println("Hi!");
+
+        abc();
+
+        try {
+            def();
+        } catch (FileNotFoundException e) {
+            System.out.println("__________________________________________");
+            System.out.println(e);
+            System.out.println("__________________________________________");
+            System.out.println(e.getMessage());
+            System.out.println("__________________________________________");
+            e.printStackTrace();
+        }
+    }
+
+    void jkl() throws FileNotFoundException{
+        abc();
+        def();
+    }
+
     public static void main(String[] args) {
+        TryCatchLearning2 t = new TryCatchLearning2();
+        t.abc();
+        t.ghi();
+//        t.def();
     }
 }
